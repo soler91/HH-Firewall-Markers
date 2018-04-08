c868f6281f84b3e2091b88d6a4033766e85d23fe
Fruit <boskozarich@gmail.com>
2018-04-08 08:44:05 -0400

    updated to the new era(?)

--- index.js
+++ index.js
@@ -1,44 +1,21 @@
 //Thanks to Riseno and Ethical for coordinates, testing and stuff.
 const	{protocol} = require('tera-data-parser'),
-		Command = require('command'),		
+        Vec3 = require('tera-vec3'),
+		Command = require('command'),
 		HARROWHOLD = 9950,
 		MARKER = 1,
 		COORDS = [
-        	{x:-7364,y:-83180,z:1},  // Front (head)
-        	{x:-8946,y:-84887,z:1},  // Right-Back leg
-		{x:-8686,y:-85301,z:1},  // Right-Back leg 2
-        	{x:-8620,y:-83531,z:1},  // Right-Front leg
-        	{x:-6667,y:-85440,z:1},  // Left-Back leg
-		{x:-7403,y:-85814,z:1},  // Left-Back leg 2
-        	{x:-6411,y:-84057,z:1}, // Left-Front leg
-		{x:-6353,y:-84872,z:1}, // Left-Middle
-		{x:-8908,y:-84001,z:1}]; // Right-Middle
-
-// Temporal packet def until tera-data updates
-if (!protocol.messages.has('S_SPAWN_BUILD_OBJECT')) {
-  const packet = [
-    ['ownerName', 'offset'],
-    ['message', 'offset'],
-    ['uid', 'uint64'],
-    ['itemId', 'uint32'],
-    ['x', 'float'],
-    ['y', 'float'],
-    ['z', 'float'],
-    ['w', 'int16'],
-    ['unk', 'uint16'],
-    ['ownerName', 'string'],
-    ['message', 'string']];
-  packet.type = 'root';
-  protocol.messages.set('S_SPAWN_BUILD_OBJECT', new Map().set(1, packet));
-}
-if (!protocol.messages.has('S_DESPAWN_BUILD_OBJECT')) {
-  const packet = [
-    ['uid', 'uint64'],
-    ['unk', 'byte']];
-  packet.type = 'root';
-  protocol.messages.set('S_DESPAWN_BUILD_OBJECT', new Map().set(1, packet));
-}
-
+        {x:-7364,y:-83180,z:1},  // Front (head)
+        {x:-8946,y:-84887,z:1},  // Right-Back leg
+	{x:-8686,y:-85301,z:1},  // Right-Back leg 2
+        {x:-8620,y:-83531,z:1},  // Right-Front leg
+        {x:-6667,y:-85440,z:1},  // Left-Back leg
+	{x:-7403,y:-85814,z:1},  // Left-Back leg 2
+        {x:-6411,y:-84057,z:1}, // Left-Front leg
+	{x:-6353,y:-84872,z:1}, // Left-Middle
+	{x:-8908,y:-84001,z:1}
+        ]; // Right-Middle
+        
 module.exports = function hhmarker(dispatch) {
 	const command = Command(dispatch)
 	let enabled = true,
@@ -63,11 +40,11 @@ module.exports = function hhmarker(dispatch) {
 		}
 	});
 	
-	dispatch.hook('S_LOGIN', 2, (event) => {
+	dispatch.hook('S_LOGIN', 9, (event) => {
 		name = event.name;
 	});
 	
-	dispatch.hook('S_LOAD_TOPO', 1, (event) => {
+	dispatch.hook('S_LOAD_TOPO', 3, (event) => {
 		ClearSpawns();
 		if(event.zone == HARROWHOLD){
 			inDung = true;
@@ -96,13 +73,11 @@ module.exports = function hhmarker(dispatch) {
 	}
 	
 	function SpawnThing(position,item){
-		dispatch.toClient('S_SPAWN_BUILD_OBJECT', 1, {
-			uid : uid,
+		dispatch.toClient('S_SPAWN_BUILD_OBJECT', 2, {
+			gameId : uid,
 			itemId : item,
-			x : position.x,
-			y : position.y,
-			z : position.z,
-			w : Math.floor((Math.random() * -30000) + 1),
+			loc : new Vec3(position),
+			w : Math.random()*Math.PI*2,
 			unk : 0,
 			ownerName : name,
 			message : ''
@@ -112,8 +87,8 @@ module.exports = function hhmarker(dispatch) {
 	}
 	
 	function Despawn(uid){
-	dispatch.toClient('S_DESPAWN_BUILD_OBJECT', 1, {
-			uid : uid,
+	dispatch.toClient('S_DESPAWN_BUILD_OBJECT', 2, {
+			gameId : uid,
 			unk : 0
 		});
 	}
